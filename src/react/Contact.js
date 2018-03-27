ca1e43a49b243fad575cda9261ef63146230b742
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2018-03-27 17:45:34 -0600

    added text and buttons to contact page

--- src/react/Contact.js
+++ src/react/Contact.js
@@ -3,8 +3,35 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div>
-      Contact
+      <div className="contact-body">
+        <div className="contact-container">
+          <div className="contact-header">Contact</div>
+          <div className="contact-text">
+            If you'd like to chat about my work, collaborating on a project, what music I'm listening to, or just say hi, feel free to reach out!
+          </div>
+          <div className="contact-links">
+            <div className="email-button">
+              <div className="fa fa-envelope"></div>
+                <div className="email">Email</div>
+              </div>
+              <div className="linkedin-button">
+                <div className="fa fa-linkedin"></div>
+                <div className="linkedin">LinkedIn</div>
+              </div>
+              <div className="facebook-button">
+                <div className="fa fa-facebook-f"></div>
+                <div className="facebook">Facebook</div>
+              </div>
+              <div className="github-button">
+                <div className="fa fa-github"></div>
+                <div className="github">Github</div>
+              </div>
+            </div>
+          </div>
+        {/* <div className="footer-container">
+          <div className="link-container">
+          </div>
+        </div> */}
       </div>
     );
   }
